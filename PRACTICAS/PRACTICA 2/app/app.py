3762bb154b4fb4b404832fd7005dab5bd22617e8
Samuel <samuelmed@correo.ugr.es>
2020-10-21 10:06:55 +0200

    COntinua el desarrollo de la segunda practica

--- PRACTICAS/PRACTICA 2/app/app.py
+++ PRACTICAS/PRACTICA 2/app/app.py
@@ -1,7 +1,26 @@
 #./app/app.py
-from flask import Flask
+from flask import Flask, url_for
+from ejercicio2 import burbuja
+import re
 app = Flask(__name__)
-    
+          
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/ordena/<elementos>')
+def ordenar(elementos):
+    print(url_for('static', filename='style.css'))
+    lista = re.findall('[0-9]',elementos)
+    aux = []
+
+    for i in range(len(lista)):
+        aux.append(int(lista[i]))
+   
+    burbuja(aux)
+    final = [str(x) for x in aux]
+    final2 = "".join(final)
+    return final2
+
+with app.test_request_context():
+    url_for('static', filename='style.css')
