cddabed8eee3310848815a6b8e1c87438524937e
Samuel <samuelmed@correo.ugr.es>
2020-10-22 17:00:12 +0200

    Practica Finalizada

--- PRACTICAS/PRACTICA 2/app/app.py
+++ PRACTICAS/PRACTICA 2/app/app.py
@@ -5,6 +5,7 @@ from ejercicios.ejercicio3 import criba
 from ejercicios.ejercicio4 import fibonacci
 from ejercicios.ejercicio5 import cadena
 import random
+import re
 app = Flask(__name__)
           
 @app.route('/')
@@ -32,6 +33,29 @@ def fibo(numero):
 def cade():
     return cadena()
 
+@app.route('/nombre/<nom>')
+def es_un_nombre(nom):
+    es_nombre = bool(re.match(r"[\w\s]+[A-Z]", nom))
+    if (es_nombre):
+        return "El nombre está bien escrito"
+    else:
+        return "El nombre no está bien escrito"
+
+@app.route('/email/<em>')
+def es_un_email(em):
+    es_email = bool(re.match(r"^[._a-z0-9-]+@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", em))
+    if (es_email):
+        return "El email está bien escrito"
+    else:
+        return "El email no está bien escrito"
+
+@app.route('/tarjeta/<num>')
+def es_una_tarjeta(num):
+    es_tarjeta = bool(re.match(r"^\d{4}([\ \-]?)\d{4}\1\d{4}\1\d{4}$", nums))
+    if (es_tarjeta):
+        return "El número de tarjeta está bien escrito"
+    else:
+        return "El número de tarjeta no está bien escrito"
 #Vamos a crear una ruta para cuando no exista la ruta a la que 
 #se está intentando acceder
 
@@ -45,13 +69,22 @@ def page_not_found(error):
 @app.route('/vectorial/')
 def imagen_vectorial_random():
     r = random.randrange(10,100) 
-    n = random.randrange(0,3)
+    n = random.randrange(0,21)
     l = r / (n+1)
     r = str(r)
     l = str(l)
-    if n == 0:
-        return '<svg>' '<circle r="' + r + ' " cx="100" cy="100" />' '</svg>' 
-    elif n == 1:
-        return '<svg>' '<rect x="60" y ="60" height="' + r + '" width = "' + r + '"/>' '</svg>'
+    if n % 4 ==  0:
+        color = "blue"
+    elif n % 4 == 1:
+        color = "red"
+    elif n % 4 == 2:
+        color = "green"
     else:
-        return '<svg>' '<ellipse cx="60" cy="60" rx="' + r + '" ry = " ' + l + '"/>' '</svg>'
+        color = "yellow"
+
+    if n % 3 == 0:
+        return '<svg>' '<circle r="' + r + ' " cx="100" cy="100" fill="' + color+ '" />' '</svg>' 
+    elif n % 3 == 1:
+        return '<svg>' '<rect x="60" y ="60" height="' + r + '" width = "' + r + '" fill="' + color + '"/>' '</svg>'
+    else:
+        return '<svg>' '<ellipse cx="60" cy="60" rx="' + r + '" ry = " ' + l + '" fill="' + color + '"/>' '</svg>'
